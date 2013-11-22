926938c7dbe9293dceca59f75a137f0e8b80575a
jprisel <a35392@alunos.isel.pt>
2013-11-22 15:14:55 +0000

    utentes

---
+++ serie1-src-tests/main/java/triagem/Utente.java
@@ -0,0 +1,17 @@
+package triagem;
+
+public class Utente {
+
+	private int numero;
+	private String nome;
+
+	public Utente(int numero, String nome){
+		this.numero = numero;
+		this.nome = nome;
+	}
+	
+	public int getNumeroUtente()
+	{
+		return numero;
+	}
+}
