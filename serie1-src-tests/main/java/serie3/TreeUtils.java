fe8e053aad2211e0d0c06cafb4072aaf092d5bf1
jprisel <a35392@alunos.isel.pt>
2013-12-16 10:04:33 +0000

    final

--- serie1-src-tests/main/java/serie3/TreeUtils.java
+++ serie1-src-tests/main/java/serie3/TreeUtils.java
@@ -14,40 +14,21 @@ public class TreeUtils {
 		if(cmp.compare(root.value, max)> 0)
 			return contains(root.left,min,max,cmp);
 		return contains(root.right,min,max,cmp);
-<<<<<<< HEAD
+
 		
 	
 	}
 	
-	public static Node <Integer> createBSTFromRange(int start, int end){
-		if(start>=end)
-			return null;
-		//int mid = (start+end)/2;
-		Node <Integer> root = new Node<Integer>();
-		createBSTAux(root, start, end);
-		return root;
-	}
-
-	private static void createBSTAux(Node<Integer> root, int start, int end) {
-=======
-	}
+	
 	
 	public static Node <Integer> createBSTFromRange(int start, int end){
->>>>>>> 07a074ac146a0fa5899a537c4d473a9630c53322
 		if(start>end)
 			return null;
 		int mid = (start+end)/2;
-<<<<<<< HEAD
-		Node <Integer> aux = new Node<Integer>(mid);
-		createBSTAux(root.left, start, mid-1);
-		createBSTAux(root.right, mid+1, end);
-		
-=======
 		Node <Integer> root = new Node<Integer>(mid);
 		root.left=createBSTFromRange(start, mid-1);
 		root.right=createBSTFromRange(mid+1, end);
 		return root;
->>>>>>> 07a074ac146a0fa5899a537c4d473a9630c53322
 	}
 
 }
--- serie1-src-tests/test/java/serie3/ContainsTest.java
+++ serie1-src-tests/test/java/serie3/ContainsTest.java
@@ -24,25 +24,25 @@ public class ContainsTest {
 	@Test
 	public void containsTest1()
 	{
-		Node <Integer> root = TreeUtilsTest.zeroToNineBST();
+		Node <Integer> root = TreeUtils.createBSTFromRange(0, 9);
 		assertFalse(contains(root, 10, 15, CMP_NATURAL_ORDER));
 	}
 	@Test
 	public void containsTest2()
 	{
-		Node <Integer> root = TreeUtilsTest.zeroToNineBST();
+		Node <Integer> root = TreeUtils.createBSTFromRange(0, 9);
 		assertTrue(contains(root, 0, 15, CMP_NATURAL_ORDER));
 	}
 	@Test
 	public void containsTest3()
 	{
-		Node <Integer> root = TreeUtilsTest.zeroToNineBST();
+		Node <Integer> root = TreeUtils.createBSTFromRange(0, 9);
 		assertFalse(contains(root, 15, 15, CMP_NATURAL_ORDER));
 	}
 	@Test
 	public void containsTest4()
 	{
-		Node <Integer> root = TreeUtilsTest.zeroToNineBST();
+		Node <Integer> root = TreeUtils.createBSTFromRange(0, 9);
 		assertTrue(contains(root, 2, 3, CMP_NATURAL_ORDER));
 	}
 	@Test
