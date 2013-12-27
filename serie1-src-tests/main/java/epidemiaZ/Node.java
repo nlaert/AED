7eb2696b2ccd4bbebf113c1a170d19afd1022f9f
jprisel <a35392@alunos.isel.pt>
2013-12-27 14:06:53 +0000

    opapapa

--- serie1-src-tests/main/java/epidemiaZ/Node.java
+++ serie1-src-tests/main/java/epidemiaZ/Node.java
@@ -1,12 +1,23 @@
 package epidemiaZ;
 
+
 public class Node<E> {
 	
 	public Node<E> next;
+	public Node<E> previous;
 	public E value;
 	
 
 	public Node(E i) {
 		value=i;
+		previous=null;
+		next=null;
+	}
+	
+	public Node()
+	{
+		value=null;
+		previous=null;
+		next=null;
 	}
 }
